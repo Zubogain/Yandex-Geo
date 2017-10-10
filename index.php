3eab913715837e84cc3343afaefd061e269852aa
Zubogain <zubogain@gmail.com>
2017-10-10 20:22:41 +0600

    Added yandex MAP

--- index.php
+++ index.php
@@ -6,6 +6,8 @@ $api = new \Yandex\Geo\Api();
 <html>
 <head>
 	<title>Сервис поиска yandex/geo</title>
+    <script src="https://api-maps.yandex.ru/2.1/?lang=ru_RU" type="text/javascript">
+    </script>
 </head>
 <body>
 <div class="search" align="center">
@@ -18,8 +20,8 @@ $api = new \Yandex\Geo\Api();
 <div class="result" align="center">
 <h3>
 <?php 
-if (isset($_GET['do_search'])) {
-    if (!empty($_GET['search'])) {
+$coords = NULL; // Массив информации о городе
+if (isset($_GET['do_search']) and !empty($_GET['search'])) {
         // Поиск по адресу
         $api->setQuery($_GET['search']);
         // Настройка фильтров
@@ -29,24 +31,55 @@ if (isset($_GET['do_search'])) {
         $response = $api->getResponse();
         // Список найденных точек
         $collection = $response->getList();
-
         if ($response->getFoundCount() == 0) {
-            echo "Ничего не найдено!";
+            echo "<p>Ничего не найдено!</p>";
         } else {
-            foreach ($collection as $item) {
-                echo $item->getAddress() . '<br>'; // вернет адрес
-                echo 'Широта: ' . $item->getLatitude() . '<br>'; // широта
-                echo 'Долгота: ' . $item->getLongitude() . '<hr>'; // долгота
+            if (count($collection) > 1) {
+                foreach ($collection as $item) {
+                echo "<a href=\"?/api={$item->getLatitude()}/{$item->getLongitude()}/{$item->getAddress()}\">" . $item->getAddress() . '</a><br>'; // вернет адрес
+                echo '<p>Широта: ' . $item->getLatitude() . '</p>'; // широта
+                echo '<p>Долгота: ' . $item->getLongitude() . '</p><hr>'; // долгота
+                }
+            } else {
+                echo "<h1>Запрос без вариантов</h1>";
+                $coords['latitude'] = $collection[0]->getLatitude();
+                $coords['longitude'] = $collection[0]->getLongitude();
+                $coords['address'] = $collection[0]->getAddress();
             }
         }
-    } else {
-        echo "Введите адрес!";
-    }
+} elseif(isset($_GET['/api']) and $pie = explode('/', $_GET['/api']) and count($pie) == 3) {
+    $coords['latitude'] = $pie[0];
+    $coords['longitude'] = $pie[1];
+    $coords['address'] = $pie[2];
 } else {
-    echo "Введите адрес!";
+    echo "<p>Введите адрес!</p>";
 }
 ?>
 </h3>
+<?php 
+if ($coords != NULL) {
+    $map = "<div id=\"map\" style=\"width: 1024px; height: 768px\"></div>
+    <script type=\"text/javascript\">
+        ymaps.ready(init);
+        var myMap, 
+            myPlacemark;
+
+        function init(){ 
+            myMap = new ymaps.Map(\"map\", {
+                center: ['{$coords['latitude']}','{$coords['longitude']}'],
+                zoom: 7
+            }); 
+            
+            myPlacemark = new ymaps.Placemark(['{$coords['latitude']}','{$coords['longitude']}'], {
+                hintContent: '{$coords["address"]}'
+            });
+            
+            myMap.geoObjects.add(myPlacemark);
+        }
+    </script>";
+    echo $map;
+}
+?>
 </div>
 </body>
 </html>
